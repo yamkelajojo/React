8f5998681bc74b39cc3c6bb8cc0b53028b81ef05
Dojo <mhlengijojo7@gmail.com>
2023-01-09 10:37:08 +0200

    Deleted everything to start over

--- react-app/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-
-// this  "element" is an Object that acts as a lightweight DOM
-const element = <h1>Hello World</h1>; 
-ReactDOM.render(element, document.getElementById("root"))
